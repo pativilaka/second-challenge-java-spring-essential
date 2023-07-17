cd98d1874d37748ed7f5fc410d644ca6a4ede483
pativilaka <p.vilaka@gmail.com>
2023-07-17 17:23:58 -0300

    Add project

---
+++ src/main/java/com/vilaka/secondchallenge/SecondChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.vilaka.secondchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vilaka/secondchallenge/SecondChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilaka.secondchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
